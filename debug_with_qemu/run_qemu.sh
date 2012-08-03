2b03bcf672d91b870681695bb89490986b5125ae
HongWoo Lee <hongwoo7@gmail.com>
2012-08-03 10:34:04 +0900

    - toolchain, qemu 설치, 확인 방법 추가. - KGDB 설정 추가.

--- debug_with_qemu/run_qemu.sh
+++ debug_with_qemu/run_qemu.sh
@@ -1,1 +1,3 @@
-qemu-system-arm -M vexpress-a9 -m 1024M -snapshot -s  -kernel ../arch/arm/boot/zImage -initrd busybox-1.20.2/rootfs.img.gz -serial stdio -append "root=/dev/ram init=/sbin/init console=ttyAMA0 debug"
+qemu-system-arm -M vexpress-a9 -m 1024M -snapshot -s  -kernel ../arch/arm/boot/zImage -initrd busybox-1.20.2/rootfs.img.gz -serial stdio -append "root=/dev/ram rdinit=/sbin/init console=ttyAMA0 debug"
+
+#qemu-system-arm -M vexpress-a9 -m 1024M -snapshot -s -S -kernel ../arch/arm/boot/zImage -initrd busybox-1.20.2/rootfs.img.gz -serial stdio -append "root=/dev/ram rdinit=/sbin/init console=ttyAMA0 debug"
